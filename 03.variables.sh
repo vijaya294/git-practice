9ecfd8d418d22cc626f1ff134dbd368c0872b65f
vijaya <vlkoduri0311@hotmail.com>
2024-08-23 18:58:59 +0100

    variables

---
+++ 03.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ram
+PERSON2=Raheem
+
+echo "$PERSON1 :: Hi $PERSON2"
+echo "$PERSON2 :: Hi $PERSON1, how are you"
+echo "$PERSON1 :: i am good $PERSON2, you"
+echo "$PERSON2 :: good $PERSON1, thank you"
