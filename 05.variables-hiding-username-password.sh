f81a50c797e2f8d13c064bfdb93140b709012b7e
vijaya <vlkoduri0311@hotmail.com>
2024-08-23 19:44:41 +0100

    variables with arguments

---
+++ 05.variables-hiding-username-password.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "please enter username :: "
+
+read -s username
+
+echo "username is :: $username"
+
+echo "please enter password :: "
+
+read -s password
+
+echo "username is :: $password"
+
