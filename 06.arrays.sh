dfa965141c719441d8bf95db7d0a2de8fd1bf6a8
vijaya <vlkoduri0311@hotmail.com>
2024-08-24 14:04:10 +0100

    arrays

---
+++ 06.arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+arrays=("apple" "orange" "kiwi")
+
+echo "first fruit is : ${array[0]}"
+echo "secong fruit is : ${array[1]}"
+echo "third fruit is : ${array[2]}"
+
+echo "all the fruits in the list are : ${array[a]}"
