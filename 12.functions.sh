5218f1d433936a54def11e38d37623c57f3b4ae1
vijaya <vlkoduri0311@hotmail.com>
2024-08-29 17:38:17 +0100

    functions

---
+++ 12.functions.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+USERID=$(id -u)   #echo "user id is : $USERID"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is failed"
+        exit 1
+    else
+        echo "$2 is success"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "you are not the root user, please log in as root user"
+    exit 1
+fi
+
+dnf list installed git
+
+VALIDATE $? "listing git"
+
+# if [ $? -ne 0 ]
+# then 
+#     echo "git is not installed, going to install it"
+
+#     dnf install git -y
+   
+#     VALIDATE $? "installing git"
+
+# else
+#     echo "git is already installed, nothong to do"
+# fi
+
+# dnf list installed mysql
+
+# if [ $? -ne 0 ]
+# then
+#     echo "mysql is not installed"
+
+#     dnf install mysql -y
+
+#     VALIDATE $? "installing mysql"
+    
+# else 
+#     echo "mysql is already installed"
+# fi
+
