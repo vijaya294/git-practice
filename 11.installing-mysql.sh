efbfa40d707b3d86e918a24565e300f4de935853
vijaya <vlkoduri0311@hotmail.com>
2024-08-29 15:38:27 +0100

    installing mysql

---
+++ 11.installing-mysql.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+USERID=(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "please log in with root privilege"
+else
+    echo "logged in as root user successfully"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #!/bin/bash
+
+# USERID=$(id -u)   #echo "user id is : $USERID"
+
+# if [ $USERID -ne 0 ]
+# then 
+#     echo "please log in as root user"
+#     exit 1
+# fi
+
+# dnf list installed git
+
+# if [ $? -ne 0 ]
+# then 
+#     echo "git is not installed, going to install it"
+
+#     dnf install git -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo "git installation is not success"
+#         exit 1
+#     else
+#         echo "git installation is success"
+#     fi
+
+# else
+#     echo "git is already installed, nothong to do"
+# fi
+
+# dnf list installed mysql
+
+# if [ $? -ne 0 ]
+# then
+#     echo "mysql is not installed"
+
+#     dnf install mywal -y
+#     if [ $? -ne 0 ]
+#     then 
+#         echo "mysql installation is failure"
+#         exit 1
+#     else 
+#         echo "mysql installation is success"
+#     fi
+
+# else 
+#     echo "mysql is already installed"
+# fi
+
