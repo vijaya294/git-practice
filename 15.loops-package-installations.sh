ceecb16f1f4c334765d454219be02abfa0ab1fa0
vijaya <vlkoduri0311@hotmail.com>
2024-08-29 19:04:40 +0100

    loops fot package installations

--- 15.loops-package-installations.sh
+++ 15.loops-package-installations.sh
@@ -8,10 +8,10 @@ N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then 
-    echo "$R Please login with root privelege $N"
+    echo -e "$R Please login with root privelege $N"
     exit 1
 else
-    echo "$G Logged as root user successfully $N"
+    echo -e "$G Logged as root user successfully $N"
 fi
 
 
