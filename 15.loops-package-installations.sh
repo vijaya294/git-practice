50221bfd522b1e6620228cf5eabc2e76f53fc208
vijaya <vlkoduri0311@hotmail.com>
2024-08-30 19:39:53 +0100

    installations

--- 15.loops-package-installations.sh
+++ 15.loops-package-installations.sh
@@ -2,66 +2,37 @@
 
 USERID=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 if [ $USERID -ne 0 ]
 then
-    echo "login with root user privilege"
+    echo "$R login with root user privilege $N"
+    exit 1
 else
-    echo "successfully logged in as root user"
+    echo "$G successfully logged in as root user"$N
 fi
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2.... $R FAILURE $N"
+        exit 1
+    else
+        echo "$2.... $G SUCCESS $N"
+    fi
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# USERID=$(id -u)
-
-# R="\e[31m"
-# G="\e[32m"
-# N="\e[0m"
-
-# if [ $USERID -ne 0 ]
-# then 
-#     echo -e "$R Please login with root privelege $N"
-#     exit 1
-# else
-#     echo -e "$G Logged as root user successfully $N"    #o/p  :  Logged as root user successfully
-
-# fi
-
-# VALIDATE(){
-#     if [ $1 -ne 0 ]
-#     then 
-#         echo "$2.... FAILED"
-#         exit 1
-#     else
-#         echo "$2.... SUCCESS"
-#     fi
-# }
-
-# for package in $@
-# do
-    
-
+for package in $@
+do
+    dnf list installed $package
+    if [ $? -ne 0 ]
+    then    
+        echo "$R $package not installed, going to install $N"
+        dnf install $package -y
+        VALIDATE $? "installng $package"
+    else
+        echo "$G $package is already installed, nothing to do $N"
+    fi
+done
