f8d06d2c6a23fc1986f9169b31b50d2a46f7761d
vijaya <vlkoduri0311@hotmail.com>
2024-08-30 19:23:14 +0100

    installations

--- 15.loops-package-installations.sh
+++ 15.loops-package-installations.sh
@@ -2,16 +2,66 @@
 
 USERID=$(id -u)
 
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-
 if [ $USERID -ne 0 ]
-then 
-    echo -e "$R Please login with root privelege $N"
-    exit 1
+then
+    echo "login with root user privilege"
 else
-    echo -e "$G Logged as root user successfully $N"
+    echo "successfully logged in as root user"
 fi
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# USERID=$(id -u)
+
+# R="\e[31m"
+# G="\e[32m"
+# N="\e[0m"
+
+# if [ $USERID -ne 0 ]
+# then 
+#     echo -e "$R Please login with root privelege $N"
+#     exit 1
+# else
+#     echo -e "$G Logged as root user successfully $N"    #o/p  :  Logged as root user successfully
+
+# fi
+
+# VALIDATE(){
+#     if [ $1 -ne 0 ]
+#     then 
+#         echo "$2.... FAILED"
+#         exit 1
+#     else
+#         echo "$2.... SUCCESS"
+#     fi
+# }
+
+# for package in $@
+# do
+    
 
