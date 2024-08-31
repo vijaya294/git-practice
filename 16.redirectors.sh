fe6df3812f064243ed680759e4851f4ff1b0f2cd
vijaya <vlkoduri0311@hotmail.com>
2024-08-31 14:51:45 +0100

    multiple packages installations

--- 16.redirectors.sh
+++ 16.redirectors.sh
@@ -16,28 +16,28 @@ N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R login with root user privilege $N" &>>LOG_FILE
+    echo -e "$R login with root user privilege $N" | tee -a LOG_FILE
     exit 1
 else
-    echo -e "$G successfully logged in as root user"$N &>>LOG_FILE
+    echo -e "$G successfully logged in as root user"$N | tee -a LOG_FILE
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e  "$2.... $R FAILURE $N" &>>LOG_FILE
+        echo -e  "$2.... $R FAILURE $N" | tee -a LOG_FILE
         exit 1
     else
-        echo -e "$2.... $G SUCCESS $N" &>>LOG_FILE
+        echo -e "$2.... $G SUCCESS $N" | tee -a LOG_FILE
     fi
 }
 
 USAGE(){
-    echo -e "$R USAGE :: sudo sh 16.redirectors.sh package1 package2 ...$N" &>>LOG_FILE
+    echo -e "$R USAGE :: sudo sh 16.redirectors.sh package1 package2 ...$N" | tee -a LOG_FILE
     exit 1
 }
 
-echo "$G script executed date and time is : $(date) $N" &>>LOG_FILE
+echo "$G script executed date and time is : $(date) $N" | tee -a LOG_FILE
 
 
 if [ $# -eq 0 ]    #here $# means to kow how many arguments are there in a file
@@ -50,10 +50,10 @@ do
     dnf list installed $package &>>LOG_FILE
     if [ $? -ne 0 ]
     then    
-        echo -e "$R $package not installed, going to install $N" &>>LOG_FILE
+        echo -e "$R $package not installed, going to install $N" | tee -a LOG_FILE
         dnf install $package -y
         VALIDATE $? "installng $package"
     else
-        echo -e "$Y $package is already installed, nothing to do $N" &>>LOG_FILE
+        echo -e "$Y $package is already installed, nothing to do $N" | tee -a LOG_FILE
     fi
 done
