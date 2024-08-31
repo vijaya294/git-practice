a938e161f445fd6dfff57438c54f5784ffebfe0d
vijaya <vlkoduri0311@hotmail.com>
2024-08-31 14:58:44 +0100

    multiple packages installations

--- 16.redirectors.sh
+++ 16.redirectors.sh
@@ -6,7 +6,7 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-mkdir -p $LOGS_FOLDER
+mkdir -p $LOGS_FOLDER 
 
 
 R="\e[31m"
