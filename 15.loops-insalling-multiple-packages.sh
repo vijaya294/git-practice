d7c7bedc084c3dc9210550e5f87be24a504037fd
vijaya <vlkoduri0311@hotmail.com>
2024-08-31 14:05:39 +0100

    multiple packages installations

--- 15.loops-insalling-multiple-packages.sh
+++ 15.loops-insalling-multiple-packages.sh
@@ -1,38 +1,55 @@
-#!/bin/bash
+  #!/bin/bash
 
 USERID=$(id -u)
 
+LOG_FOLDER="/var/logs/shell-sript"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+
+
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R login with root user privilege $N"
+    echo -e "$R login with root user privilege $N" &>>LOG_FILE
     exit 1
 else
-    echo -e "$G successfully logged in as root user"$N
+    echo -e "$G successfully logged in as root user"$N &>>LOG_FILE
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e  "$2.... $R FAILURE $N"
+        echo -e  "$2.... $R FAILURE $N" &>>LOG_FILE
         exit 1
     else
-        echo -e "$2.... $G SUCCESS $N"
+        echo -e "$2.... $G SUCCESS $N" &>>LOG_FILE
     fi
 }
 
+USAGE(){
+    echo -e "$R $USAGE :: sudo sh 15.loops-insalling-multiple-packages.sh package1 package2 ...$N" &>>LOG_FILE
+    exit 1
+}
+
+if [ $USAGE -eq 0 ]
+then
+    USAGE
+fi
+
 for package in $@
 do
-    dnf list installed $package
+    dnf list installed $package &>>LOG_FILE
     if [ $? -ne 0 ]
     then    
-        echo -e "$R $package not installed, going to install $N"
+        echo -e "$R $package not installed, going to install $N" &>>LOG_FILE
         dnf install $package -y
         VALIDATE $? "installng $package"
     else
-        echo -e "$G $package is already installed, nothing to do $N"
+        echo -e "$Y $package is already installed, nothing to do $N" &>>LOG_FILE
     fi
 done
